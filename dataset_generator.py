1c207ebbb29d93ecd75d01d6e43e9fb9bb90ad74
Oded Yechiel <odedyech@post.bgu.ac.il>
2018-07-04 21:07:33 +0300

    Data reader complete

--- dataset_generator.py
+++ dataset_generator.py
@@ -10,13 +10,34 @@ from keras.layers import *
 from keras.optimizers import SGD
 from scipy.stats.stats import pearsonr
 from keras.regularizers import *
-
+import numpy as np
 
 def oneHot(string):
-	for x in range(len(string), 35):
-		string = string+'A'
+	# for x in range(len(string)):
+		# string = string+'A'
 	trantab=maketrans('ACGT','0123')
-	string=string+'ACGT'
+	# string=string+'ACGT'
 #	data=list(string.translate({ord('A'):'0',ord('C'):'1',ord('G'):'2',ord('T'):'3'}))
 	data=list(string.translate(trantab))
-	return to_categorical(data)[0:-4]
+	data = map(int, data[0:60])
+	return to_categorical(data)
+
+
+def pbm_dataset_generator(filename):
+	f = open(filename, 'r')
+	data = []
+	for line in f:
+		data.append(oneHot(line))
+	f.close()
+	return data
+
+def selex_dataset_generator(filename):
+	f = open(filename, 'r')
+	data = []
+	labels = []
+	for line in f:
+		line2 = line.split('\t')
+		data.append(oneHot(line2[0]))
+		labels.append(int(line2[1]))
+	f.close()
+	return data, labels
--- main.py
+++ main.py
@@ -5,35 +5,34 @@ import sys
 
 
 
-if len(sys.argv) < 4:
-    print "Length of input arguments is ", len(sys.argv)
-    print "Usage:\n python main.py train/predict pbm_file SELEX_FILE_0 SELEX_FILE_1 ..."
-    sys.exit(0)
+def parse_args():
+    if len(sys.argv) < 4:
+        print "Length of input arguments is ", len(sys.argv)
+        print "\nUsage:\n python main.py pbm_file SELEX_FILE_0 SELEX_FILE_1 ..."
+        print "\nUsage2:\n python main.py pbm_file #of_selex_0 #of_selex_1 ..."
+        sys.exit(0)
+
+    PBM_FILE = sys.argv[1]
+    SELEX_FILES = [sys.argv[i] for i in range(2, len(sys.argv))]
+    if SELEX_FILES[0].isdigit():
+        SELEX_FILES = map(int, SELEX_FILES)
+    return PBM_FILE, SELEX_FILES
 
 
-TRAIN = True if "train" in sys.argv[1] else False
-PREDICT = True if "predict" in sys.argv[1] else False
-
-if not (TRAIN or PREDICT):
-    print sys.argv
-    print "Usage:\n python main.py train/predict pbm_file SELEX_FILE_0 SELEX_FILE_1 ..."
-    sys.exit(0)
-
-PBM_FILE = sys.argv[2]
-SELEX_FILES = [sys.argv[i] for i in range(3, len(sys.argv))]
-
-if TRAIN:
-    print "Training on:"
-    print PBM_FILE
-    print "Using: "
-    print SELEX_FILES
-
-elif PREDICT:
-    print "Predicting on:"
-    print PBM_FILE
-    print "Using: "
-    print SELEX_FILES
+def main(PBM_FILE, SELEX_FILES):
+    if len(SELEX_FILES) < 2:
+        parse_args()
+    if type(SELEX_FILES[0]) == int:
+        base = PBM_FILE.split('_')[0]
+        selex = [base+'_selex_'+str(i)+'.txt' for i in SELEX_FILES]
+    pbm_data = pbm_dataset_generator(PBM_FILE)
+    selex_4, cnt4  = selex_dataset_generator(selex[4])
+    print len(selex_4)
 
 
 
+if __name__ == '__main__':
+    # PBM_FILE, SELEX_FILES = parse_args()
+    # main(PBM_FILE, SELEX_FILES)
+    main('train/TF1_pbm.txt', [0, 1, 2, 3, 4])
 
