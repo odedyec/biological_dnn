345b08c5bb9ab03afebd0d9c9405d7dbbae8be3d
Serious programmer <odedyech@post.bgu.ac.il>
2018-07-29 09:51:34 +0300

    Testing on entire dataset - result file inside avg. 16

--- dataset_generator.py
+++ dataset_generator.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 import time
 
-TRAIN_SIZE = 400000
-TEST_SIZE = 400000
+TRAIN_SIZE = 100000
+TEST_SIZE = 100000
 
 def label_generator(num_of_labels, size):
 	"""
